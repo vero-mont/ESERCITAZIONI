7a90b0810c4ef7863f472a0a59a6f5fe3e71f6e6
vero-mont <veronicamonteleone07@gmail.com>
2026-06-11 20:17:51 +0200

    operaori2

---
+++ ES_3_JS/operatori2.js
@@ -0,0 +1,10 @@
+let anno_corrente = 2026;
+let anno_nascita = 2004;
+
+let eta = anno_corrente - anno_nascita;
+
+console.log("L'età è: " + eta);
+
+let quanto_manca_100 =  100 - eta;
+
+console.log("Mancano " + quanto_manca_100 + " anni per raggiungere i 100 anni.");
