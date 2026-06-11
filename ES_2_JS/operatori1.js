1045821dbfc1b07b393bc98514959409d06d41ec
vero-mont <veronicamonteleone07@gmail.com>
2026-06-11 20:00:59 +0200

    operatori1

---
+++ ES_2_JS/operatori1.js
@@ -0,0 +1,13 @@
+let num1 = 10; // Dichiarazione di una variabile numerica
+let num2 = 5;  // Dichiarazione di un'altra variabile numerica
+let num3 = 2;  // Dichiarazione di una terza variabile numerica
+let num4 = 3;  // Dichiarazione di una quarta variabile numerica
+let num5 = 4;  // Dichiarazione di una quinta variabile numerica
+
+let somma = num1 + num2 + num3 + num4 + num5; 
+console.log("La somma è: " + somma); 
+
+let media = somma / 5;
+console.log("La media è: " + media);
+
+console.log("la somma di questi numeri equivale a: " + somma + " e la media è: " + media);
