f271802ebed4753328cba2dde1901763d726ea3d
vero-mont <veronicamonteleone07@gmail.com>
2026-06-11 19:45:07 +0200

    variabilijs

---
+++ ES_1_JS/variabili.js
@@ -0,0 +1,15 @@
+let num1 = 5;
+let num2 = 10;
+console.log(num1);
+console.log(num2);
+console.log(num1 + num2);
+
+
+let stringa = "string";
+
+stringa = "buongiorno";
+console.log(stringa);
+
+const PIGRECO = 3.14159265359;
+console.log(PIGRECO);
+PIGRECO = 3.50; 
