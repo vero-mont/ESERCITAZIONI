4375815622822ebd18037fc05e91c6b387d07cb2
vero-mont <veronicamonteleone07@gmail.com>
2026-06-12 10:51:24 +0200

    operatori 3

---
+++ ES_4_JS/operatori3.js
@@ -0,0 +1,16 @@
+let var1 = prompt("scrivi un numero");
+let var2 = prompt("scrivi un altro numero");
+
+
+let somma= (parseInt(var1) + parseInt(var2));
+let sottrazione= (parseInt(var1) - parseInt(var2));
+let moltiplicazione= (parseInt(var1) * parseInt(var2));
+let divisione= (parseInt(var1) / parseInt(var2));   
+let potenza= (parseInt(var1) ** parseInt(var2));
+
+console.log("Con i numeri da te scelti i risultati sono : ");
+console.log("Somma  : " + somma);
+console.log("Sottrazione : " + sottrazione);
+console.log("Moltiplicazione  : " + moltiplicazione);
+console.log("Divisione  : " + divisione);
+console.log("Potenza  : " + potenza);
